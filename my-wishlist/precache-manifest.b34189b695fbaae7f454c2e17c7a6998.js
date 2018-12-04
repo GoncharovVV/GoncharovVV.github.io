adb9708048197365da3f77ea2bd8c8801e6c696d
GoncharovVV <goncharov.v.v2017@gmail.com>
2018-12-04 22:11:41 +0300

    first

---
+++ my-wishlist/precache-manifest.b34189b695fbaae7f454c2e17c7a6998.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "575b0f8ebcd6a2f10004",
+    "url": "/my-wishlist/static/css/main.fb13b992.chunk.css"
+  },
+  {
+    "revision": "575b0f8ebcd6a2f10004",
+    "url": "/my-wishlist/static/js/main.575b0f8e.chunk.js"
+  },
+  {
+    "revision": "aa2f1978925e5305e053",
+    "url": "/my-wishlist/static/js/1.aa2f1978.chunk.js"
+  },
+  {
+    "revision": "d1d6c626bc3dcbfce35a",
+    "url": "/my-wishlist/static/js/runtime~main.d1d6c626.js"
+  },
+  {
+    "revision": "0380cc8d2a7982b5d9dfd190026e2e46",
+    "url": "/my-wishlist/index.html"
+  }
+];
